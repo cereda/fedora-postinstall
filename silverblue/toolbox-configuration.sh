51d231fcbee1dd7101dd7414a6cf5d188323396b
Paulo Roberto Massa Cereda <cereda.paulo@gmail.com>
2025-05-01 13:24:22 -0300

    Added trap for toolbox

--- silverblue/toolbox-configuration.sh
+++ silverblue/toolbox-configuration.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+function toolbox_handle_error {
+    echo "Error occurred while creating and configuring toolbox. Please verify."
+}
+
 section "Toolbox configuration"
 
 describe "Toolbox is a command-line tool that provides a convenient way \
@@ -12,4 +16,7 @@ echo
 TOOLBOX_NAME=$(${GUM} input --prompt "Your toolbox container name: " --value "f${FEDORA_VERSION}-dev")
 
 info "Creating and configuring toolbox."
+trap 'toolbox_handle_error' ERR
 toolbox create ${TOOLBOX_NAME}
+unset -f toolbox_handle_error
+trap - ERR
