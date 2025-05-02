6baecd066e198327093b83d939bcecceb759b1c2
Paulo Roberto Massa Cereda <cereda.paulo@gmail.com>
2025-05-02 14:35:11 -0300

    Typo in fallback

--- silverblue/useful-packages.sh
+++ silverblue/useful-packages.sh
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-PACKAGE_INSTALL_LIST="No packages provided. This is a clean toolbox."
+PACKAGE_INSTALL_LIST=" No packages provided. This is a clean toolbox."
 
 section "Useful packages"
 
