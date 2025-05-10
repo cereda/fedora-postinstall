4c7887478e031308512b88a4abd8e859fbebeebf
Paulo Roberto Massa Cereda <cereda.paulo@gmail.com>
2025-05-10 09:54:20 -0300

    Documented entry point

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -24,9 +24,13 @@
 
 SCRIPT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
+# 'gum' is required, so in case it's not available, the script will download
+# a fixed version from GitHub
 source "common/gum-setup.sh"
 source "common/gum-functions.sh"
 
+# get the current version of Fedora by expanding the '%fedora' macro, which
+# returns an integer
 FEDORA_VERSION=$(rpm -E %fedora)
 
 chapter "Paulo's Fedora ${FEDORA_VERSION} post installation script"
@@ -39,29 +43,52 @@ distribution."
 
 echo
 
+# check for a special environment variable which will skip the entire post
+# installation and move directly to the additional command line tools
 if [ -z "${CLI_TOOLS_ONLY+x}" ]; then
 
+    # no special variable found, proceed to the post installation
+
+    # it's important to ugrade your system prior to running this script; 
+    # however, Workstation users could get rid of certain packages first
+    # in order to save several MB in updates
     question "Did you upgrade your system?" || exit 1
 
+    # load the list of suggested flatpaks and fonts to install -- since
+    # these settings are common to both flavours of Fedora, they are
+    # loaded at this stage
     source "common/flatpaks-to-install.sh"
     source "common/fonts-to-install.sh"
 
     text "Welcome to my post installation script for Fedora! \
 Please select which flavour of Fedora you are currently running."
 
+    echo
+
     FEDORA_FLAVOUR=$(${GUM} choose "Workstation" "Silverblue")
 
     text "You are running Fedora ${FEDORA_FLAVOUR} ${FEDORA_VERSION}. Let's go!"
 
+    # load the corresponding post installation script based the flavour of 
+    # Fedora; note to self: in the future, make this check automatic so the
+    # script will already know about the underlying operating system
     [[ "${FEDORA_FLAVOUR}" = "Workstation" ]] && source "workstation/post-installation.sh"
     [[ "${FEDORA_FLAVOUR}" = "Silverblue" ]] && source "silverblue/post-installation.sh"
 else
 
-text "Welcome to my post installation script for Fedora! It appears you have \
-chosen to skip the flavour selection. The script will now proceed directly \
-to the installation of additional command line tools."
+    # special environment variable found, so the entire post installation
+    # is skipped; print a message to indicate this to the user
+
+    text "Welcome to my post installation script for Fedora! It appears you \
+have chosen to skip the flavour selection. The script will now proceed \
+directly to the installation of additional command line tools."
 fi
 
+# load the script for installing the additional command line tools; it should
+# not have any dependencies on the post installation settings
 source "extras/additional-tools.sh"
 
-text "That's all, folks!"
+echo
+
+# end of script, we did it!
+text "That's all, folks! Have fun with Fedora!"
